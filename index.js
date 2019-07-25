5f06b88ff063006f89cbfedfea13f5e8990a48b9
Brian Simonton <bsimonton78@gmail.com>
2019-07-25 11:33:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,3 +32,14 @@ class Neighborhood {
 }
 
 
+class Customer {
+  constructor(name, neighborhoodId){
+    this.name = name;
+    this.neighborhoodId = neighborhoodId;
+    this.id = ++customerId;
+    store.customers.push(this);
+  }
+  
+  
+  
+}
